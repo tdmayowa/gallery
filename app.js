81c629e932f58a0f3f0e803fe295f69e058c71e7
tdm <taiwodaniel1998@gmail.com>
2020-09-21 14:08:55 +0100

    More improvement and the beginning of code cleaning up II

--- app.js
+++ app.js
@@ -1,79 +1,60 @@
 const express = require("express");
+var app = express();
 const ejs     = require("ejs");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
-const smtpTransport = require("nodemailer-smtp-transport");
-// var fancybox = require('@fancyapps/fancybox');
+const mongoose    = require("mongoose");
+const { ConnectionStates } = require("mongoose");
+const path = require ("path");
+const flash       = require("connect-flash");
+const passport    = require("passport");
+const localStrategy = require("passport-local");
+const Image  = require("./models/image");
+const User = require("./models/user");
+var middleware = require("./middleware/index");
 
-var app = express();
+var image = require("./routes/image");
+var index = require("./routes/index");
 
+
+var url = "mongodb://localhost/mux_v4";
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+
+//This line tells points makes public directory the default for all styles
+app.use(express.static(__dirname + "/public")); 
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded(
     {
         extended: true
-    })
-);
+    }
+));
+app.use(flash());
 
-//route to get the home page
-app.get("/", function(req, res){
-    res.render("home");
+
+//passport config
+app.use(require("express-session")({
+    secret: "I really am wholly_tdm",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//passing currentUser tol every of the templates
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
 });
 
-// route to get the gallery page
-app.get("/gallery", function(req, res){
-    res.render("images")
-});
+//Using all routes connected to the app.js file
+app.use(image);
+app.use(index);
 
-// route to get the contact us page
-app.get("/contact-us", function(req, res){
-    res.render("contact_us");
-});
-
-//route that handles the logical part of the messsage post request
-app.post("/", function(req, res){
-    console.log(req.body.name);
-    console.log(req.body.email);
-    console.log(req.body.msg);
-    var name = req.body.name,
-        email = req.body.email,
-        message = req.body.msg;
-
-    const mailer = async (options) => {
-        const fastaMailer = await nodemailer.createTransport(smtpTransport({
-                service: "gmail",
-                host: "smtp.gmail.com",
-                auth: 
-                {
-                        user: "taiwodaniel1998@gmail.com", // gmail used just for testing purposes
-                        pass: "08087016021" // for testing purposes
-                }
-        }));
-
-        const mailOptions = {
-                from: "<taiwodaniel1998@gmail.com>",
-                to: 'taiwodaniel2020@gmail.com',
-                subject: 'Message to Mux_Media',
-                text: message + " from " + name + " with email " +  email
-        };
-
-        await fastaMailer.sendMail(mailOptions, (error) => {
-                // console.log(mailOptions, info);
-                if (error) {
-                        throw error;
-                        // console.log(error);
-                }
-                return "Mail sent";
-        });
-        res.redirect("/contact-us");
-    };
-    mailer();
-
-})
-
-app.get("/about-mux", function(req, res){
-    res.render("about_us"); 
-});
 
 
 var port = process.env.PORT || 8080;
